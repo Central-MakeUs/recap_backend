adaca3607c042c11115fb3d644db8df41f7f863f
woogym <kwj9294@gmail.com>
2026-06-23 16:56:21 +0900

    feat: initial commit

---
+++ src/test/java/cmc/recap/RecapApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
