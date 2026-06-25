cbd1e8c564de17501a0d0173b384311378017329
woogym <kwj9294@gmail.com>
2026-06-25 19:07:58 +0900

    fix: add missing bean injection annotation

--- src/main/java/cmc/recap/global/config/SwaggerConfig.java
+++ src/main/java/cmc/recap/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,6 +15,7 @@ public class SwaggerConfig {
     @Value("${swagger.server.url}")
     private String serverUrl;
 
+    @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
