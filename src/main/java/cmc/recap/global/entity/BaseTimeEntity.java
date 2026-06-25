9181b6e64f1b8b4e69c190d49df39b590f935a28
woogym <kwj9294@gmail.com>
2026-06-26 00:54:52 +0900

    chore/#3: add BaseTimeEntity class

---
+++ src/main/java/cmc/recap/global/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package cmc.recap.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private Instant modifiedAt;
+}
