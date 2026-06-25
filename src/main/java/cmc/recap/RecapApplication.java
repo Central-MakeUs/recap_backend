52e99c714125a197250b016b68c77d20f87e7c39
woogym <kwj9294@gmail.com>
2026-06-26 00:59:12 +0900

    chore/#3: stuck JVM time zones explicitly as UTC

--- src/main/java/cmc/recap/RecapApplication.java
+++ src/main/java/cmc/recap/RecapApplication.java
@@ -1,5 +1,6 @@
 package cmc.recap;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecapApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(RecapApplication.class, args);
     }
-
 }
