9ca68cccf26454d86c8c4fa04c1ac8476bd39c71
woogym <kwj9294@gmail.com>
2026-07-05 02:19:34 +0900

    feat: User 엔티티 소셜 로그인을 위한 필드 추가(#5)

--- src/main/java/cmc/recap/user/domain/User.java
+++ src/main/java/cmc/recap/user/domain/User.java
@@ -1,5 +1,6 @@
 package cmc.recap.user.domain;
 
+import cmc.recap.global.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,12 +8,16 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,7 +33,34 @@ public class User {
     @Column(name = "fcm_token")
     private String fcmToken;
 
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "oauth_provider")
     private String oauthProvider;
+
+    @Column(name = "oauth_id")
     private String oauthId;
+
+    private User(String deviceId, Platform platform) {
+        this.deviceId = deviceId;
+        this.platform = platform;
+    }
+
+    public static User createByDevice(String deviceId, Platform platform) {
+        return new User(deviceId, platform);
+    }
+
+    public void linkOauth(String email, String oauthProvider, String oauthId) {
+        if (this.oauthId != null) {
+            throw new IllegalStateException("이미 소셜 계정이 연결된 유저입니다.");
+        }
+        this.email = email;
+        this.oauthProvider = oauthProvider;
+        this.oauthId = oauthId;
+    }
+
+    public void updateFcmToken(String fcmToken) {
+        this.fcmToken = fcmToken;
+    }
 }
